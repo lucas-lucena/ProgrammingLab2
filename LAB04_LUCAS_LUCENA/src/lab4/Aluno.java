d9d6c7f2df0260cea2688e35feb4a2258367e70f
Lucas Lucena <lucas.lucena@ccc.ufcg.edu.br>
2019-04-23 23:29:42 -0300

    Alguns dos primeiros metodos.

--- LAB04_LUCAS_LUCENA/src/lab4/Aluno.java
+++ LAB04_LUCAS_LUCENA/src/lab4/Aluno.java
@@ -2,9 +2,21 @@ package lab4;
 
 public class Aluno {
 
-	
-	
-	private String  matricula;
+	private String matricula;
 	private String nome;
 	private String curso;
+
+	public Aluno(String matricula, String nome, String curso) {
+
+		this.matricula = matricula;
+		this.nome = nome;
+		this.curso = curso;	
+	}
+	
+	public String toString() {
+		return "Aluno: " + this.matricula +
+				" - " + this.nome +
+				" - " + this.curso;
+	}
+	
 }
--- LAB04_LUCAS_LUCENA/src/lab4/AlunoController.java
+++ LAB04_LUCAS_LUCENA/src/lab4/AlunoController.java
@@ -1,9 +1,50 @@
 package lab4;
 
+import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class AlunoController {
 
-	private HashMap<String, Aluno> MapaAlunos = new HashMap<>();
-
+	private HashMap<String, Aluno> MapaAlunos;
+	private HashMap<String, HashSet> MapaGrupos;
+	
+	public AlunoController() {
+		this.MapaAlunos = new HashMap<>();
+	}
+	
+	public String CadastraAluno(String matricula, String nome, String curso) {
+		
+		if  (MapaAlunos.containsKey(matricula)) {
+			return "MATRÍCULA JÁ CADASTRADA!";
+		}
+		else {
+			Aluno aluno = new Aluno(matricula, nome, curso);
+			MapaAlunos.put(matricula, aluno);
+			return "CADASTRO REALIZADO!";
+		}
+	}
+	
+	public String ConsultaAluno(String matricula) {
+		
+		if (MapaAlunos.containsKey(matricula)) {
+			return MapaAlunos.get(matricula).toString();
+		}
+		else {
+			return "Aluno não cadastrado.";
+		}
+	}
+	
+	public String CadastraGrupo(String nomeGrupo) {
+		nomeGrupo = nomeGrupo.toLowerCase();
+		
+		if (MapaGrupos.containsKey(nomeGrupo)) {
+			return "GRUPO JÁ CADASTRADO!";
+		}
+		else {
+			HashSet<Aluno> grupoSet = new HashSet<>();
+			MapaGrupos.put(nomeGrupo, grupoSet);
+			return "CADASTRO REALIZADO!";
+		}
+	}
 }
